951f4418fb711227b325d54a90133f9e44209c92
Manseerat Kaur <seeratsidhu01@gmail.com>
2022-03-18 22:59:47 -0400

    Show options for autocomplete

--- client/src/components/IngredientSearch.js
+++ client/src/components/IngredientSearch.js
@@ -1,6 +1,21 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import IngredientList from "./IngredientList";
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+
+const resultsArray = [
+		{
+			"id": 9003,
+			"name": "apple",
+			"image": "apple.jpg"
+		},
+		{
+			"id": 9019,
+			"name": "applesauce",
+			"image": "applesauce.png"
+		}
+]
 
 export default function IngredientSearch() {
   const [term, setTerm] = useState('');
@@ -8,33 +23,46 @@ export default function IngredientSearch() {
 
   // const URL = `https://api.spoonacular.com/food/ingredients/search?query=${term}&number=2&apiKey=${process.env.REACT_APP_API_KEY}`
 
-  useEffect(() => {
-    axios
-      .get(URL)
-      .then(function (response) {
-        setResults(response.data.results);
-        console.log(results);
-      })
-      .catch(function (error) {
-        console.warn(error);
-      });
-  }, [term]);
+  // useEffect(() => {
+  //   axios
+  //     .get(URL)
+  //     .then(function (response) {
+  //       setResults(response.data.results);
+  //       console.log(results);
+  //     })
+  //     .catch(function (error) {
+  //       console.warn(error);
+  //     });
+  // }, [term]);
   
+  console.log(term)
+  
+  const handleChange = (value) => {
+    if (!value) {
+      setResults([]);
+    } else {
+      setResults(resultsArray)
+    }
 
+    setTerm(value);
+  }
  
   return (
     <main>
-      <form onSubmit={(event) => event.preventDefault()}>
-        <input
-          className="ingredient_search-input text--semi-bold"
+      <Form onSubmit={(event) => event.preventDefault()}>
+        <Form.Control
+          size="lg"
           name="name"
           type="text"
           placeholder="Seach Ingredients"
           value={term}
-          onChange={(event) => setTerm(event.target.value)}
-        />
-      </form>
-     <IngredientList results = {results}/>
+          onChange={(event) => handleChange(event.target.value)}
+          />
+      </Form>
+      {results && results.map((result) => {
+        return <Card key = {result["id"]} body> {result["name"]}</Card>
+       
+      })}
     </main>
   );
 }
