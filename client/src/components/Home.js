6f764e0396c92edac453ded0990f9508795b7f41
JuliaaMuller <julialm.muller@gmail.com>
2022-03-19 21:45:00 -0400

    work on navbar home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,60 @@
-import React from "react";
-import NavMenu from "./NavMenu";
+import React, { useState } from 'react';
+import NavMenu from './NavMenu';
+import './Home.scss';
+import { GrAddCircle } from 'react-icons/gr';
+import { ButtonGroup, ToggleButton } from 'react-bootstrap';
 
-function Home () {
+function Home (props) {
+
+    const [dayValue, setdayValue] = useState('1');
+  
+    const days = [
+      { name: 'Mon', value: '1' },
+      { name: 'Tue', value: '2' },
+      { name: 'Wed', value: '3' },
+      { name: 'Thu', value: '4' },
+      { name: 'Fri', value: '5' },
+      { name: 'Sat', value: '6' },
+      { name: 'Sun', value: '7' },
+    ];
   return (
-    <><NavMenu />
-    <h1>
-        Home Page
-      </h1></>
-
-
+    <><NavMenu />  
+  <main>
+    <h4>
+        Welcome to Freedge - We will help you free your fridge.
+    </h4>
+    <div className='my-ingredients'>
+         What's in your fridge ? 
+         <br/>
+         <GrAddCircle  className='go-to-ingredients' onClick={props.onClick}/>
+    </div>
+    <div className='my-meal-planner'>
+         Your meal planner
+         <br/>
+         <ButtonGroup className='days-button'>
+        {days.map((day, idx) => (
+          <ToggleButton
+            key={idx}
+            id={`day-${idx}`}
+            type='day'
+            variant="outline-secondary"
+            name='days'
+            value={day.value}
+            checked={dayValue === day.value}
+            onChange={(e) => setdayValue(e.currentTarget.value)}
+          >
+            {day.name}
+          </ToggleButton>
+        ))}
+      </ButtonGroup>
+    </div>
+  </main>
+      
+      
+      </>
   )
 }
+
 
 
 export default Home;
--- client/src/components/NavMenu.js
+++ client/src/components/NavMenu.js
@@ -1,24 +1,33 @@
 import * as React from 'react';
 import './NavMenu.scss';
 import { Navbar, NavDropdown, Container, Nav } from 'react-bootstrap';
+import { BiFridge, BiBookHeart, BiCalendar, BiSearchAlt, BiLogOut } from 'react-icons/bi';
+import { MdOutlineAddShoppingCart } from 'react-icons/md';
+import { BsPersonCircle } from 'react-icons/bs';
+import { AiOutlineSetting } from 'react-icons/ai';
 
 
 export default function NavMenu() {
   return (
-<Navbar bg="light" expand="lg">
+<Navbar  bg="light" expand="lg">
+
   <Container>
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Brand href="#home">< BiFridge className="icon-nav"/> Freedge</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
         <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        <NavDropdown className ="dropdown-nav" title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <Nav.Link href="#link">Sign in</Nav.Link>
+        <NavDropdown className ="dropdown-nav" title="Menu" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1"><BiFridge/> My ingredients</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2"><MdOutlineAddShoppingCart/> My shopping list</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3"><BiCalendar/> My meal planner</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.4"><BiBookHeart/> My favorites</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.5"><BiSearchAlt/> Explore</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.6"><BsPersonCircle/> About</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.7"><AiOutlineSetting/> Setting</NavDropdown.Item>
           <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.8"><BiLogOut/> Log out</NavDropdown.Item>
         </NavDropdown>
       </Nav>
     </Navbar.Collapse>
