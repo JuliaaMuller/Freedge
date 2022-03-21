22cff5afec89e8dbe276d66957a060eed6799188
JuliaaMuller <julialm.muller@gmail.com>
2022-03-21 13:59:15 -0400

    create register and setting page / the about us page

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import NavMenu from './NavMenu';
 import { Form, FormGroup, FormControl, FormCheck, Button } from 'react-bootstrap';
+import { RiLockPasswordLine } from 'react-icons/ri';
+import { MdAlternateEmail } from 'react-icons/md';
 
 function LogIn (props) {
   return (
@@ -9,7 +11,7 @@ function LogIn (props) {
   <h2>Log in </h2>
   <Form>
   <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
+    <Form.Label><MdAlternateEmail/> Email address</Form.Label>
     <Form.Control type="email" placeholder="Enter email" />
     <Form.Text className="text-muted">
       {/* We'll never share your email with anyone else. */}
@@ -17,13 +19,13 @@ function LogIn (props) {
   </Form.Group>
 
   <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
+    <Form.Label><RiLockPasswordLine/> Password</Form.Label>
     <Form.Control type="password" placeholder="Password" />
   </Form.Group>
   <Form.Group className="mb-3" controlId="formBasicCheckbox">
     <Form.Check type="checkbox" label="Remember my info" />
   </Form.Group>
-  <Button variant="primary" type="submit">
+  <Button variant="btn btn-outline-secondary" type="submit">
     Login
   </Button>
 </Form>
--- client/src/components/Settings.js
+++ client/src/components/Settings.js
@@ -1,11 +1,57 @@
 import React, { useState } from 'react';
 import NavMenu from './NavMenu';
+import { Form, FormGroup, FormControl, FormCheck, Button } from 'react-bootstrap';
+import { MdAlternateEmail } from 'react-icons/md';
+import { BiUserCircle, BiPhone } from 'react-icons/bi';
+import { RiLockPasswordLine } from 'react-icons/ri';
+import { BsHouseDoor } from 'react-icons/bs';
 
 function Settings (props) {
   return (
 <><NavMenu /> 
 <main>
   <h2>Settings</h2>
+  <p> Did you need to update some informations ? </p>
+
+  <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label><MdAlternateEmail/> Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter Email" />
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label><RiLockPasswordLine/> Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicFirstName">
+    <Form.Label><BiUserCircle/> First Name</Form.Label>
+    <Form.Control type="firstName" placeholder="First Name" />
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicLastName">
+    <Form.Label><BiUserCircle/> Last Name</Form.Label>
+    <Form.Control type="lastName" placeholder="Last Name" />
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicAddress">
+    <Form.Label><BsHouseDoor/> Your address</Form.Label>
+    <Form.Control type="address" placeholder="Adress" />
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPhoneNumber">
+    <Form.Label><BiPhone/> Your phone number</Form.Label>
+    <Form.Control type="phoneNumber" placeholder="Phone Number" />
+  </Form.Group>
+
+  <Button variant="btn btn-outline-secondary" type="submit">
+      Edit
+    </Button>
+
+    <Button variant="btn btn-outline-secondary" type="submit">
+      Save
+    </Button>
+  </Form>
 </main>
 </>
   )
