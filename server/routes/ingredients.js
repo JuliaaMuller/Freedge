34e5448305ac8071a29a43c4971444ab1d34ee89
Manseerat Kaur <seeratsidhu01@gmail.com>
2022-03-22 12:44:32 -0400

    generate recipes api based on ingredients

--- server/routes/ingredients.js
+++ server/routes/ingredients.js
@@ -14,10 +14,19 @@ function insertIntoDb(category, user_id, db) {
 module.exports = (db) => {
   // all routes will go here
   router.get("/", (req, res) => {
-    const command = "SELECT * FROM ingredients";
-    db.query(command).then((data) => {
-      res.json(data.rows);
-    });
+    const command = "SELECT array_agg(name) AS name, array_agg(quantity) AS quantity, category FROM ingredients WHERE user_id = 1 GROUP BY category;";
+    db.query(command).then(data => {
+     
+      let result = {};
+
+      for (let item of data.rows) {
+        const ingredientList = item["name"].map((name, index) => ({name: name, quantity: item["quantity"][index]}));
+        result[item["category"]] = ingredientList
+      }
+      
+      res.json(result);
+
+    })
   });
 
   router.post("/", (req, res) => {
