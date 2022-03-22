3bc225d316379713970cf554b76fd9dfa1be397a
Manseerat Kaur <seeratsidhu01@gmail.com>
2022-03-22 10:35:12 -0400

    ingredients API

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -5,10 +5,20 @@ const router = require('express').Router();
 module.exports = (db) => {
   // all routes will go here 
   router.get('/', (req, res) => {
-    const command = "SELECT * FROM ingredients";
+    const command = "SELECT array_agg(name) AS name, array_agg(quantity) AS quantity, category FROM ingredients WHERE user_id = 1 GROUP BY category;";
     db.query(command).then(data => {
-      res.json(data.rows);
+     
+      let result = {};
+
+      for (let item of data.rows) {
+        const ingredientList = item["name"].map((name, index) => ({name: name, quantity: item["quantity"][index]}));
+        result[item["category"]] = ingredientList
+      }
+      
+      res.json(result);
+
     })
+
   });
 
   return router;
